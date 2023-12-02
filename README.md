1a804633baa8bcce2dccc3e8fbcb8af7785d4799
Luka Ivanović <luka@ljudi.org>
2023-12-02 13:07:12 +0100

    Add readme and licence
    Decided to used 2-clause BSD licence for this.

---
+++ README.md
@@ -0,0 +1,6 @@
+# advent-of-code-2023
+
+## Licence
+
+This code is licensed under the 2-clause BSD licence (see LICENCE for details).
+
--- day2/main.go
+++ day2/main.go
@@ -1,3 +1,6 @@
+// Copyright © 2023 Luka Ivanović
+// This code is licensed under the 2-clause BSD licence (see LICENCE for details)
+
 package main
 
 import (
